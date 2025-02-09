db58dce18615abb64760792bf5f3f94490351d2b
Leonardo Sawamoto <l.sawamoto@uniandes.edu.co>
2025-02-09 11:45:16 -0500

    Basic Structure

---
+++ player.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+
+import rclpy
+from rclpy.node import Node
+from geometry_msgs.msg import Twist
+import sys
+import tty
+import termios
+import select
+
+
+class TutleBotPlayer():
+
+    def __init__(self):
+        pass
+
+def main(args=None):
+    rclpy.init(args=args)
+    node = TutleBotPlayer()
+    rclpy.shutdown()
+
+if __name__ == '__main__':
+    main()
